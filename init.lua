2cc68a0983cdc79935edb9dd25093f593d1de072
David Cox <dcox@bmc.com>
2017-09-14 09:19:45 -0500

    Add a timer loop so we don't exit

--- init.lua
+++ init.lua
@@ -1,6 +1,7 @@
 local os = require('os')
 local io = require('io')
 local json = require('json')
+local timer = require('timer')
 
 --
 -- Read and parse the plugin file.
@@ -28,3 +29,10 @@ file:write("Home : " .. os.getenv("HOME") .. "\n")
 file:write("Plugin params : " .. foo_var .. "\n")
 file:close()
 
+--Dummy function to be called after every polling interval.
+--
+function dummy()
+	--Do Nothing.
+end
+
+timer.setInterval(300000, dummy)
